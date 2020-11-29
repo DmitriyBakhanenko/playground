22980993b1fff344766a274aa103f8cdfe55dea6
Dmitriy Bakhanenko <bahanenko@gmail.com>
2020-11-29 23:35:47 +0200

    redux playground

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { selectSong } from '../actions';
-import SongList from './SongList';
-import Player from './Player';
 
-const App = ({ songs, SongToPlayer, selectSongDispatch }) => {
-  const onSongSelect = song => selectSongDispatch(song);
-
+export default function App() {
   return (
-    <div className='ui grid'>
-      <div className='eight wide column'>
-        <SongList songs={songs} onSongSelect={onSongSelect} />
-      </div>
-      <div className='eight wide column'>
-        {SongToPlayer ? <Player SongToPlayer={SongToPlayer} /> : null}
-      </div>
+    <div>
+      <div>FDFDFSDFSDFSD</div>
     </div>
   );
-};
-
-const dispatchStateToProps = dispatch => ({
-  selectSongDispatch: song => dispatch(selectSong(song))
-});
-
-const mapStateToProps = ({ songs, selectedSong }) => ({
-  songs: songs,
-  SongToPlayer: selectedSong
-});
-
-export default connect(mapStateToProps, dispatchStateToProps)(App);
+}
