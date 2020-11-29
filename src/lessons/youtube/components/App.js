1431b83f12ccde7a73bebcfe2363e32860d884fa
Dmitriy Bakhanenko <bahanenko@gmail.com>
2020-11-29 16:48:14 +0200

    updated youtube class based components to functional based with react hooks

--- src/lessons/youtube/components/App.js
+++ src/lessons/youtube/components/App.js
@@ -1,51 +1,42 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
 
-import SearchBar from './lessons/youtube/components/SearchBar';
-import { youtubeApi } from './lessons/youtube/components/Youtube';
-import VideoList from './lessons/youtube/components/VideoList';
-import VideoDetail from './lessons/youtube/components/VideoDetail';
+import SearchBar from './SearchBar';
+import { youtubeApi } from './Youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-export default class App extends Component {
-  state = {
-    items: [],
-    selectedVideo: null
-  };
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  onTermSubmit = async term => {
-    const response = await youtubeApi.get('/search', {
+  const onTermSubmit = async term => {
+    const { data } = await youtubeApi.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({
-      items: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
+    setItems(data.items);
+    setSelectedVideo(data.items[0]);
   };
 
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div style={{ width: '70vw', margin: '20px auto' }}>
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className='ui grid'>
-          <div className='ui row'>
-            <div className='eleven wide column'>
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className='five wide column'>
-              <VideoList
-                videos={this.state.items}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  return (
+    <div style={{ width: '70vw', margin: '20px auto' }}>
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className='ui grid'>
+        <div className='ui row'>
+          <div className='eleven wide column'>
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className='five wide column'>
+            <VideoList
+              videos={items}
+              onVideoSelect={e => setSelectedVideo(e.target.value)}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
--- src/lessons/youtube/components/SearchBar.js
+++ src/lessons/youtube/components/SearchBar.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class SearchBar extends Component {
-  state = { term: '' };
+const SearchBar = ({ onTermSubmit }) => {
+  const [term, setTerm] = useState('');
 
-  onSubmitForm = event => {
+  const onSubmitForm = event => {
     event.preventDefault();
 
-    this.props.onTermSubmit(this.state.term);
-    this.setState({ term: '' });
+    onTermSubmit(term);
+    setTerm('');
   };
 
-  render() {
-    return (
-      <div className='ui segment'>
-        <form className='ui form' onSubmit={this.onSubmitForm}>
-          <div className='ui icon input' style={{ width: '100%' }}>
-            <input
-              type='text'
-              placeholder='Search...'
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
-            />
-            <i className='search icon'></i>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='ui segment'>
+      <form className='ui form' onSubmit={onSubmitForm}>
+        <div className='ui icon input' style={{ width: '100%' }}>
+          <input
+            type='text'
+            placeholder='Search...'
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+          <i className='search icon'></i>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
