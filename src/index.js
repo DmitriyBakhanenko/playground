8242431474c1aabfd634a8ea36925fe962f04b93
Dmitriy Bakhanenko <bahanenko@gmail.com>
2020-12-03 16:04:52 +0200

    relocation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-
-import App from './components/App';
-import rootReducer from './reducers';
-
-import './index.css';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <App /> */}
+    <h1>HI</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/lessons/porslist/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import rootReducer from './reducers';
+
+import './index.css';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
